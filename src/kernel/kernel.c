b9294f356d44e2be850005e19ebfe1b16ef6ee8e
Aarav Mehta <axrxvm@gmail.com>
2026-02-21 20:52:46 +0530

    Revert "feat: implement core kernel/fs TODOs and add HTTPS policy controls"
    This reverts commit 7d64027a473d3f1614aa81c383904fbf5142fbfa.

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -527,7 +527,7 @@ mount_done:
     register_component_task("subsystem.userspace", TASK_TYPE_SUBSYSTEM, PRIORITY_NORMAL);
     userspace_init();
     
-    // Launch userspace shell in the bootstrap execution context.
+    // Launch userspace shell (TODO: should be a separate process)
     userspace_run();
     
     // Should never reach here
